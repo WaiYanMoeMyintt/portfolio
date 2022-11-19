7d1da43025247605053931beb12ca6b693dcd537
WaiYanMoeMyint <wymoemyint4523@gmail.com>
2022-11-19 20:07:55 +0630

    Update animation

--- js/script.js
+++ js/script.js
@@ -47,8 +47,8 @@ menu.addEventListener('click',(m)=>{
 ScrollReveal().reveal('main', {delay:700, duration:1000, interval:4});
 ScrollReveal().reveal('nav',{delay:500, duration:1200, scale:1.5, interval:4 ,origin:"bottom"});
 
-ScrollReveal().reveal('.profile-info-left', {delay:600,duration:1000,distance:"20rem",origin:"left",interval:5,easing:"ease"});
-ScrollReveal().reveal('.profile-info-right', {delay:600,duration:1000,distance:"20rem",origin:"right",interval:5,easing:"ease" });
+ScrollReveal().reveal('.profile-info-left', {delay:600,duration:1000,origin:"left",interval:5,easing:"ease"});
+ScrollReveal().reveal('.profile-info-right', {delay:600,duration:1000,origin:"right",interval:5,easing:"ease" });
 
 ScrollReveal().reveal('.info-section-left', {delay:600,duration:1500,interval:2,origin:"left",easing:"ease"});
 ScrollReveal().reveal('.info-section-right', {delay:600,duration:1500,interval:2,origin:"left",easing:"ease" });
@@ -64,8 +64,8 @@ ScrollReveal().reveal('.service-1', {delay:"500",duration:"1000",distance:"10rem
 ScrollReveal().reveal('.service-2', {delay:"600",duration:"1200",distance:"10rem",origin:"right",easing:"ease",scale:1.1 });
 ScrollReveal().reveal('.service-3', {delay:"700",duration:"1400",distance:"10rem",origin:"top",easing:"ease",scale:1.3 });
 
-ScrollReveal().reveal('.footer-contact', {delay:"100",duration:"1000",distance:"20rem",origin:"right",easing:"ease" });
-ScrollReveal().reveal('.footer-control', {delay:"1200",duration:"1300",distance:"20rem",origin:"left",easing:"ease"});
+ScrollReveal().reveal('.footer-contact', {delay:"100",duration:"1000",origin:"right",easing:"ease" });
+ScrollReveal().reveal('.footer-control', {delay:"1200",duration:"1300",origin:"left",easing:"ease"});
 
 
 // ScrollReveal().reveal('main', {delay:700, duration:1000});
@@ -76,25 +76,25 @@ ScrollReveal().reveal('.footer-control', {delay:"1200",duration:"1300",distance:
 // {delay:"700",easing:"ease-in",useDelay:"always"});
 
 
-// ScrollReveal().reveal('.profile-info-left', {delay:"700",duration:"1000",distance:"20rem",origin:"left",interval:500,easing:"ease"});
-// ScrollReveal().reveal('.profile-info-right', {delay:"700",duration:"1000",distance:"20rem",origin:"right",interval:500,easing:"ease" });
+// ScrollReveal().reveal('.profile-info-left', {delay:"700",duration:"1000",origin:"left",interval:500,easing:"ease"});
+// ScrollReveal().reveal('.profile-info-right', {delay:"700",duration:"1000",origin:"right",interval:500,easing:"ease" });
 
-// ScrollReveal().reveal('.info-section-left', {delay:"700",duration:"1000",distance:"20rem",origin:"left",easing:"ease"});
-// ScrollReveal().reveal('.info-section-right', {delay:"700",duration:"1000",distance:"20rem",origin:"right",easing:"ease" });
+// ScrollReveal().reveal('.info-section-left', {delay:"700",duration:"1000",origin:"left",easing:"ease"});
+// ScrollReveal().reveal('.info-section-right', {delay:"700",duration:"1000",origin:"right",easing:"ease" });
 
-// ScrollReveal().reveal('.skill-section', {delay:"700",duration:"1000",distance:"20rem",origin:"left",easing:"ease",scale:1.1 });
+// ScrollReveal().reveal('.skill-section', {delay:"700",duration:"1000",origin:"left",easing:"ease",scale:1.1 });
 
 // // ScrollReveal().reveal('.project-section', {delay:"800",duration:"1000",easing:"ease" });
-// ScrollReveal().reveal('.chillflix', {delay:"800",duration:"1500",distance:"20rem",origin:"left",easing:"ease" });
-// ScrollReveal().reveal('.bank', {delay:"1000",duration:"1800",distance:"20rem",origin:"right",easing:"ease" });
-// ScrollReveal().reveal('.shortern', {delay:"1200",duration:"2100",distance:"20rem",origin:"left",easing:"ease" });
-// ScrollReveal().reveal('.modern', {delay:"1400",duration:"2400",distance:"20rem",origin:"right",easing:"ease" });
+// ScrollReveal().reveal('.chillflix', {delay:"800",duration:"1500",origin:"left",easing:"ease" });
+// ScrollReveal().reveal('.bank', {delay:"1000",duration:"1800",origin:"right",easing:"ease" });
+// ScrollReveal().reveal('.shortern', {delay:"1200",duration:"2100",origin:"left",easing:"ease" });
+// ScrollReveal().reveal('.modern', {delay:"1400",duration:"2400",origin:"right",easing:"ease" });
 
-// // ScrollReveal().reveal('.services-section', {delay:"1200",duration:"1000",distance:"20rem",origin:"left",easing:"ease" });
-// ScrollReveal().reveal('.service-1', {delay:"1000",duration:"1200",distance:"20rem",origin:"left",easing:"ease",scale:1.05 });
-// ScrollReveal().reveal('.service-2', {delay:"1300",duration:"1800",distance:"20rem",origin:"left",easing:"ease",scale:1.1 });
-// ScrollReveal().reveal('.service-3', {delay:"1500",duration:"2500",distance:"20rem",origin:"left",easing:"ease",scale:1.3 });
+// // ScrollReveal().reveal('.services-section', {delay:"1200",duration:"1000",origin:"left",easing:"ease" });
+// ScrollReveal().reveal('.service-1', {delay:"1000",duration:"1200",origin:"left",easing:"ease",scale:1.05 });
+// ScrollReveal().reveal('.service-2', {delay:"1300",duration:"1800",origin:"left",easing:"ease",scale:1.1 });
+// ScrollReveal().reveal('.service-3', {delay:"1500",duration:"2500",origin:"left",easing:"ease",scale:1.3 });
 
 
-// ScrollReveal().reveal('.footer-contact', {delay:"1300",duration:"1800",distance:"20rem",origin:"right",easing:"ease" });
-// ScrollReveal().reveal('.footer-control', {delay:"1500",duration:"2000",distance:"20rem",origin:"left",easing:"ease"});
+// ScrollReveal().reveal('.footer-contact', {delay:"1300",duration:"1800",origin:"right",easing:"ease" });
+// ScrollReveal().reveal('.footer-control', {delay:"1500",duration:"2000",origin:"left",easing:"ease"});
